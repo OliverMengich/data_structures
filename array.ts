243e04d53ca5a7f0687b57b3ca51d0550b38211c
OliverMengich <kemeioliver2000@gmail.com>
2023-02-21 05:19:15 +0300

    Arrays in DSA

--- array.ts
+++ array.ts
@@ -1,1 +1,10 @@
 const strings: string[] = ["a","b","c","d"];
+
+strings.push('e') //O(1)
+strings.pop() //O(1)
+strings.unshift('i','ii') //O(n) ->loops through array to assign new indices
+//unshift can't be best
+
+strings.splice(2,1,'f') //O(n)
+console.log(strings);
+ 
